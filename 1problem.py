df102d42fad613b5ae156128a93e739a279cec20
engelspencer <sengel@sjnma.org>
2018-01-10 14:01:39 -0600

    problem 1 complete

---
+++ 1problem.py
@@ -0,0 +1,15 @@
+import math
+
+totaltime = input('Enter any number of seconds: ')
+
+hours = int(totaltime/3600)
+totaltime = totaltime%3600
+minutes = int(totaltime/60)
+totaltime = totaltime%60
+seconds = totaltime
+
+print(str(hours) + ':' + str(minutes) + ':' + str(seconds))
+
+a = input('Enter the edge length of a dodecahedron: ')
+area = 3 * math.sqrt(25 + (10 * math.sqrt(5))) * a * a
+print (str(area) + 'square units')
